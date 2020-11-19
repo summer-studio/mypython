756b859171ef3bd0795a899b10c37a9d76031432
bin <mango2049@163.com>
2020-11-19 09:15:37 +0800

    update

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+print('hello world')
+print('hello', 'world')
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+
+print(100 + 200)
+print('100 + 200=', 100 + 200)
+
