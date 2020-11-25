f3298c2d1edbc809756e548996c99fc02bb089e6
bin <mango2049@163.com>
2020-11-25 09:06:13 +0800

    update

--- main.py
+++ main.py
@@ -16,10 +16,3 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-print('hello world')
-print('hello', 'world')
-print('The quick brown fox', 'jumps over', 'the lazy dog')
-
-print(100 + 200)
-print('100 + 200=', 100 + 200)
-
