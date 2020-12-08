c818d94474994ecce68b82c409cff62583056952
bin <mango2049@163.com>
2020-12-08 20:42:30 +0800

    update

--- functional/do_map.py
+++ functional/do_map.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from functools import reduce
+
+
 def f(x):
     return x * x
 
 
 r = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 print(list(r))
+
+
+def add(x, y):
+    return x + y
+
+
+a = reduce(add, [1, 3, 5, 7, 9])
+print(a)
